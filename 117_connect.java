9ed300ecf78fb0ff2dd8f0670db09ea34b8119b3
fukaiyu <fukaiyu.214@bytedance.com>
2022-01-20 10:30:10 +0800

    2022.01.19

---
+++ 117_connect.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node layerRoot = root;
+        while (layerRoot != null) {
+            Node cur = layerRoot;
+            Node prev = null;
+            layerRoot = null;
+            while (cur != null) {
+                if (cur.left != null) {
+                    if (prev == null) {
+                        layerRoot = cur.left;
+                    } else {
+                        prev.next = cur.left;
+                    }
+                    prev = cur.left;
+                }
+                if (cur.right != null) {
+                    if (prev == null) {
+                        layerRoot = cur.right;
+                    } else {
+                        prev.next = cur.right;
+                    }
+                    prev = cur.right;
+                }
+                cur = cur.next;
+            }
+        }
+        return root;
+    }
+}
