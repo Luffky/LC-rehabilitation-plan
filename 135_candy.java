43de9b10562a38bbfa370e1c5a6851ffde190739
fukaiyu <fukaiyu.214@bytedance.com>
2022-02-10 16:00:35 +0800

    2022.02.10

--- 135_candy.java
+++ 135_candy.java
@@ -2,8 +2,8 @@ class Solution {
     public int candy(int[] ratings) {
         int prev = 0;
         int[] counts = new int[ratings.length];
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] >= ratings[i - 1]) {
+        for (int i = 1; i <= ratings.length; i++) {
+            if (i == ratings.length || ratings[i] >= ratings[i - 1]) {
                 if (i - 1 == prev) {
                     counts[prev] = prev == 0 || ratings[prev] == ratings[prev - 1] ? 1 : counts[prev - 1] + 1;
                 } else {
@@ -15,16 +15,6 @@ class Solution {
                 }
                 prev = i;
             } 
-        }
-        int i = ratings.length;
-        if (i - 1 == prev) {
-            counts[prev] = prev == 0 || ratings[prev] == ratings[prev - 1] ? 1 : counts[prev - 1] + 1;
-        } else {
-            counts[i - 1] = 1;
-            for (int j = i - 2; j > prev; j--) {
-                counts[j] = counts[j + 1] + 1;
-            }
-            counts[prev] = prev == 0 || ratings[prev] == ratings[prev - 1] ? counts[prev + 1] + 1 : Math.max(counts[prev + 1], counts[prev - 1]) + 1;
         }
         int sum = 0;
         for (int j = 0; j < counts.length; j++) {
