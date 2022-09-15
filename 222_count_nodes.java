62e0e5195a2f5bbbb97c281be94787dbcd0c8d7e
fukaiyu <fukaiyu.214@bytedance.com>
2022-09-15 20:25:55 +0800

    fix 2022.09.15

--- 222_count_nodes.java
+++ 222_count_nodes.java
@@ -14,56 +14,33 @@
  * }
  */
 class Solution {
-    LinkedList<Boolean> list;
-    int totalLevel;
     public int countNodes(TreeNode root) {
-        list = new LinkedList<>();
         if (root == null) {
             return 0;
         }
-        //计算层数
-        totalLevel = 0;
-        TreeNode curNode = root;
-        while (curNode != null) {
-            curNode = curNode.left;
-            totalLevel++;
-        }
-        //除最后一层外的节点个数
+        //计算总层数
+        int totalLevel = mostBottomLevel(root, 0);
+        //计算除最后一层外的节点个数
         int totalCount = (int) Math.pow(2, totalLevel - 1) - 1;
-        //二分法开始找最后一层的最右节点位置
-        findRightestNode(root, 1);
+        //计算总结点数
         int count = (int) Math.pow(2, totalLevel - 2);
-        for (boolean isRight : list) {
-            if (isRight) {
-                totalCount += count;
-            }
-            count /= 2;
-        }
-        totalCount++;
-        return totalCount;
+        return findRightestNode(root, 1, count, totalCount, totalLevel);
     }
-    public boolean findRightestNode(TreeNode root, int curLevel) {
-        if (root.left == null) {
-            return curLevel == totalLevel;
-        } else if (root.right == null) {
-            list.addLast(false);
-            boolean has = findRightestNode(root.left, curLevel + 1);
-            if (!has) {
-                list.removeLast();
-            }
-            return has;
-        } else {
-            list.addLast(true);
-            if (findRightestNode(root.right, curLevel + 1)) {
-                return true;
-            }
-            list.removeLast();
-            list.addLast(false);
-            boolean has = findRightestNode(root.left, curLevel + 1);
-            if (!has) {
-                list.removeLast();
-            }
-            return has;
+    public int findRightestNode(TreeNode root, int curLevel, int count, int totalCount, int totalLevel) {
+        if (root.left == null && root.right == null) {
+            return totalCount + 1;
         }
+        if (mostBottomLevel(root.right, curLevel) == totalLevel) {
+            return findRightestNode(root.right, curLevel + 1, count / 2, totalCount + count, totalLevel);
+        } else {
+            return findRightestNode(root.left, curLevel + 1, count / 2, totalCount, totalLevel);
+        }
+    }
+    public int mostBottomLevel(TreeNode root, int curLevel) {
+        while (root != null) {
+            root = root.left;
+            curLevel++;
+        }
+        return curLevel;
     }
 }
